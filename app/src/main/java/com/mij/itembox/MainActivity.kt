c583f9fbc079fe1d796b66ec67579b400728feed
sweder25 <ji.maldonado@duocuc.cl>
2025-10-15 19:40:57 -0300

    Android studio

--- app/src/main/java/com/mij/itembox/MainActivity.kt
+++ app/src/main/java/com/mij/itembox/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hell $name Insane mode!",
         modifier = modifier
     )
 }
